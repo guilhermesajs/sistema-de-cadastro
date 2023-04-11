bdb7922a3b37bc38f8a4196b865bec9465efb277
Guilherme dos Santos Araújo <guilherme.silva@ituniverse.ad>
2023-04-11 11:25:31 -0300

    Voltando para o estado anterior

--- app.js
+++ app.js
@@ -1,4 +1,1 @@
-console.log("Rodando o sistema de gerenciamento de jogos")
-console.log("aplicação está se conectando ao banco de dados")
-// mais um console log de teste:
-console.log("Enviando para o GitHub")
+console.log"Rodando o sistema de gerenciamento de jogos"
